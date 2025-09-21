a63aea51c450106cda303e20bc6ba7c6a5c00e8d
jorgelzz <jorgeluiz2027@hotmail.com>
2025-09-21 19:40:42 -0300

    minor changes with keys

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,9 +1,20 @@
 import React from "react"
+import { Outlet } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 
 const AdminLayout = () => {
 
     return (
-        <div className="admin-layout">Admin Layout</div>
+        <div className="admin-layout">
+            MobileSideBar
+            <aside className="w-full h-full max-w-[270px] hidden
+            lg:block">
+                <SidebarComponent></SidebarComponent>
+            </aside>
+            <aside className="children">
+                <Outlet />
+            </aside>
+        </div>
     )
 }
 
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -3,7 +3,7 @@ const Dashboard = () => {
 
     return (
         <div>
-            <h1>Admin Dashboard</h1>
+            Dashboard Page
         </div>
     )
 }
